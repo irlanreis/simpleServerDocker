8e6a62343981111152002a8364269c40a3c4c209
irlanreis <xuiexpress@gmail.com>
2025-04-11 09:52:14 -0400

    fix: adiciona cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes.js');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/users', userRoutes);
 
